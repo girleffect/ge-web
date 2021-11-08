ed17cbeed0e16cf55e98cf00cb26ed5c4b1a04cb
Kaitlyn Crawford <kaitlyn@praekelt.com>
2021-11-08 20:01:53 +0200

    Add template tag that displays footers at the bottom of every page

---
+++ home/templatetags/home_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from home.models import FooterPage
+
+register = template.Library()
+
+
+@register.inclusion_tag('home/tags/footer_page_list.html', takes_context=True)
+def footer_pages(context):
+    pages = []
+    request = context['request']
+    site = request._wagtail_site
+    if site:
+        pages = FooterPage.objects.descendant_of(site.root_page)
+
+    return {
+        'request': request,
+        'footers': pages,
+    }
