1e5abfcf0fabc6e480767f1592270b65c5b8af76
Codie Roelf <codiebeulaine@gmail.com>
2021-11-24 20:53:35 +0200

    black

--- articles/templatetags/home_tags.py
+++ articles/templatetags/home_tags.py
@@ -17,15 +17,17 @@ def footer_pages(context):
         "footers": pages,
     }
 
-@register.inclusion_tag('articles/tags/breadcrumbs.html', takes_context=True)
+
+@register.inclusion_tag("articles/tags/breadcrumbs.html", takes_context=True)
 def breadcrumbs(context):
-    self = context.get('self')
+    self = context.get("self")
     ancestors = []
     if self is not None and not self.depth <= 2:
-        ancestors = Page.objects.live().ancestor_of(
-            self, inclusive=True).filter(depth__gt=3)
+        ancestors = (
+            Page.objects.live().ancestor_of(self, inclusive=True).filter(depth__gt=3)
+        )
 
     return {
-        'ancestors': ancestors,
-        'request': context['request'],
+        "ancestors": ancestors,
+        "request": context["request"],
     }
