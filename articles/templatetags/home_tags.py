d7328d76cdb4693f0fe824ebba7ec48320f096b1
Codie Roelf <codiebeulaine@gmail.com>
2021-11-24 20:52:25 +0200

    add breadcrumbs

--- articles/templatetags/home_tags.py
+++ articles/templatetags/home_tags.py
@@ -4,7 +4,7 @@ from articles.models import FooterPage
 register = template.Library()
 
 
-@register.inclusion_tag("home/tags/footer_page_list.html", takes_context=True)
+@register.inclusion_tag("articles/tags/footer_page_list.html", takes_context=True)
 def footer_pages(context):
     pages = []
     request = context["request"]
@@ -15,4 +15,17 @@ def footer_pages(context):
     return {
         "request": request,
         "footers": pages,
+    }
+
+@register.inclusion_tag('articles/tags/breadcrumbs.html', takes_context=True)
+def breadcrumbs(context):
+    self = context.get('self')
+    ancestors = []
+    if self is not None and not self.depth <= 2:
+        ancestors = Page.objects.live().ancestor_of(
+            self, inclusive=True).filter(depth__gt=3)
+
+    return {
+        'ancestors': ancestors,
+        'request': context['request'],
     }
