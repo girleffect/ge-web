1e4ce04fcbabba4ac38d1a4e29f74c70d4c45419
Codie Roelf <codiebeulaine@gmail.com>
2021-11-24 18:57:18 +0200

    update modeladmin

--- profiles/wagtail_hooks.py
+++ profiles/wagtail_hooks.py
@@ -4,13 +4,18 @@ from .models import GEUser
 
 class GEUserAdmin(ModelAdmin):
     model = GEUser
-    menu_label = "User"  # ditch this to use verbose_name_plural from model
+    menu_label = "Users"  # ditch this to use verbose_name_plural from model
     menu_icon = "user"  # change as required
     menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = (
         False  # or True to exclude pages of this type from Wagtail's explorer view
     )
+    list_display = ("user", "gender", "location", "date_of_birth")
+    list_export = ("user", "gender", "location", "date_of_birth")
+    list_filter = ("gender", "date_of_birth")
+    export_filename = "users"
+    search_fields = ("gender", "location", "date_of_birth")
 
 
 # Now you just need to register your customised ModelAdmin class with Wagtail
