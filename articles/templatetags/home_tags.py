d119c3c9044b961786ae5c118ba2299ce6f89862
Codie Roelf <codiebeulaine@gmail.com>
2021-11-30 16:52:19 +0200

    add form errors

--- articles/templatetags/home_tags.py
+++ articles/templatetags/home_tags.py
@@ -8,8 +8,10 @@ register = template.Library()
 @register.inclusion_tag("home/tags/footer_page_list.html", takes_context=True)
 def footer_pages(context):
     request = context["request"]
+    pages = []
     site = Site.find_for_request(request)
-    pages = FooterPage.objects.descendant_of(site.root_page)
+    if site:
+        pages = FooterPage.objects.descendant_of(site.root_page)
     return {
         "request": request,
         "footers": pages,
