96779f0b95ac9214cea53ff1de3b1a972f1e0e21
Mitso <mqalaba@gmail.com>
2021-12-19 17:21:03 +0200

    update section article teasers

--- geweb/static/js/springster.js
+++ geweb/static/js/springster.js
@@ -1,7 +1,22 @@
-import "../styles/springster/springster.scss";
+import "../styles/springster/springster.scss"; 
 
-// SECTION CLASS NAMES THEME
-const sectionAnchor = document.querySelectorAll('.nav-list__anchor')
-sectionAnchor.forEach(element => {
-    element.classList.add('nav-list__' + element.innerText.toLowerCase().replace(' ', '-'));
-});
+
+/**
+ * Header search from toggle 
+ */
+const elemHeaderSearchToggle = document.getElementById('search'),
+    elemSearchBar = document.querySelector('.search--header > .search__bar');
+elemSearchBar.style.display = 'none'
+elemSearchBar.style.visibility = 'hidden'
+elemHeaderSearchToggle.addEventListener('click', (e) => { 
+
+    if (elemSearchBar.style.display === 'none') {
+        elemSearchBar.style.display = 'block'
+        elemSearchBar.style.visibility = 'visible'
+        e.target.style.background = 'url("http://127.0.0.1:8000/static/img/springster/dismiss.svg") no-repeat 3px 5px/auto 80%'
+    } else {
+        elemSearchBar.style.display = 'none'
+        elemSearchBar.style.visibility = 'hidden'
+        e.target.style.background = 'url("http://127.0.0.1:8000/static/img/springster/nav_search.svg") no-repeat 3px 0/auto 95%'
+    }
+})
