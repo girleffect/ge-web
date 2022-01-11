a661b4e764ebdee815342b1133cf62ba345fdd3d
Mitso <mqalaba@gmail.com>
2022-01-11 18:28:49 +0200

    upate profiles

--- geweb/static/js/zathu.js
+++ geweb/static/js/zathu.js
@@ -1,4 +1,20 @@
 import "../styles/zathu/zathu.scss";
 
 
-console.log('Hello world');
+/**
+ * Forms
+ */
+
+const terms_input = document.getElementById('id_terms_and_conditions'),
+    terms_label = document.querySelector('label[for="id_terms_and_conditions"]')
+if (terms_input) {
+    terms_input.addEventListener('change', function(e) {
+        if (e.target.checked) {
+            e.target.classList.add('is-on')
+            terms_label.classList.add('is-on')
+        } else {
+            e.target.classList.remove('is-on')
+            terms_label.classList.remove('is-on')
+        }
+    })
+}
