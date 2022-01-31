79783128c41826c006b900563ee96cb2cca30620
Codie Roelf <codiebeulaine@gmail.com>
2022-01-31 07:49:44 +0200

    fix flake8

--- home/models.py
+++ home/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from modelcluster.contrib.taggit import ClusterTaggableManager
-from modelcluster.fields import ParentalKey
-from taggit.models import TaggedItemBase
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.core import blocks
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
-from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.search import index
 
 from articles.models import SectionPage
 from forms.models import FormPage
