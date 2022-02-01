11e08e5fbd2e2c1f578bd80e351ca09ba7d74f0b
Kaitlyn Crawford <kaitlyn@praekelt.com>
2022-02-01 19:39:15 +0200

    Add missing import

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.core import blocks
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from articles.models import ArticlePage, SectionPage
