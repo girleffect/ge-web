850d2be491b1c0a557e193e967d1021772c4faf9
Mitso <mqalaba@gmail.com>
2022-02-06 11:52:38 +0200

    update homepage with 2 banner positions

--- geweb/static/js/tujibebe.js
+++ geweb/static/js/tujibebe.js
@@ -1,1 +1,24 @@
 import "../styles/tujibebe/tujibebe.scss";
+
+
+
+/**
+ * Header search from toggle 
+ */
+ const elemHeaderSearchToggle = document.getElementById('search'),
+    elemSearchBar = document.querySelector('.search--header > .search__bar')
+    elemSearchBar.style.display = 'none'
+    elemSearchBar.style.visibility = 'hidden'
+    elemHeaderSearchToggle.addEventListener('click', (e) => { 
+
+    if (elemSearchBar.style.display === 'none') {
+        elemSearchBar.style.display = 'block'
+        elemSearchBar.style.visibility = 'visible'
+        //elemHeaderSearchToggle.style.backgroundPosition = '-165px -240px'
+        console.log(e.target)
+    } else {
+        elemSearchBar.style.display = 'none'
+        elemSearchBar.style.visibility = 'hidden'
+        //elemHeaderSearchToggle.style.backgroundPosition = '0 -177px'
+    }
+})
