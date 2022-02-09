792e68f3ef3562116669dd0a767a338bcf5e2f0b
Codie Roelf <codiebeulaine@gmail.com>
2022-02-09 21:58:24 +0200

    flake8

--- search/views.py
+++ search/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
 from django.core.paginator import Paginator
-from articles.models import ArticlePage
+from django.shortcuts import render
+from wagtail.core.models import Site
 from wagtail.search.models import Query
+
+from articles.models import ArticlePage
 
 
 def search(request):
-    search_query = request.GET.get('query', None)
+    search_query = request.GET.get("query", None)
     if search_query:
         site = Site.find_for_request(request)
-        results = ArticlePage.objects.live().descendant_of(site.root_page).search(search_query)
+        results = (
+            ArticlePage.objects.live()
+            .descendant_of(site.root_page)
+            .search(search_query)
+        )
         search_results = Paginator(results, 10)
         # Log the query so Wagtail can suggest promoted results
         Query.get(search_query).add_hit()
@@ -16,9 +22,13 @@ def search(request):
         search_results = ArticlePage.objects.none()
 
     # Render template
-    page_number = request.GET.get('page', '1')
+    page_number = request.GET.get("page", "1")
     page_obj = search_results.get_page(page_number)
-    return render(request, 'search_results.html', {
-        'search_query': search_query,
-        'search_results': page_obj,
-    })
+    return render(
+        request,
+        "search_results.html",
+        {
+            "search_query": search_query,
+            "search_results": page_obj,
+        },
+    )
