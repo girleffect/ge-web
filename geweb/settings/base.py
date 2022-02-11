3e66c936bca02bc7a4042da5aab36213948a79f1
Mitso <mqalaba@gmail.com>
2022-02-11 05:26:07 +0200

    update format

--- geweb/settings/base.py
+++ geweb/settings/base.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "wagtail.contrib.simple_translation"
+    "wagtail.contrib.simple_translation",
 ]
 
 MIDDLEWARE = [
