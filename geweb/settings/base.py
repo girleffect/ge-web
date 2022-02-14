e3673d16a58f1aa86c83b9f32515dac1454c21d6
Kaitlyn Crawford <kaitlyn@praekelt.com>
2022-02-14 14:27:33 +0200

    Add support for storing media in S3

--- geweb/settings/base.py
+++ geweb/settings/base.py
@@ -205,3 +205,16 @@ WAGTAIL_CONTENT_LANGUAGES = LANGUAGES = [
 TAGGIT_CASE_INSENSITIVE = True
 
 LOGIN_URL = "/profiles/login/"
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+AWS_DEFAULT_ACL = "public-read"
+
+if AWS_STORAGE_BUCKET_NAME and AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY:
+    MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    INSTALLED_APPS += [
+        "storages",
+    ]
