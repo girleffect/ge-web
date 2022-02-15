58b17517cd5cb40470f711608800df4f3db042dc
Codie Roelf <codiebeulaine@gmail.com>
2022-02-15 23:43:50 +0200

    udpate

--- articles/management/commands/import_locales.py
+++ articles/management/commands/import_locales.py
@@ -3,9 +3,6 @@ import json
 from django.core.management.base import BaseCommand
 from wagtail.core.models import Locale
 
-from articles.models import SectionIndexPage
-from home.models import HomePage
-
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -15,22 +12,3 @@ class Command(BaseCommand):
                 locale, created = Locale.objects.get_or_create(
                     language_code=langs[lang]["locale"]
                 )
-
-        default_home = HomePage.objects.first()
-        for locale in Locale.objects.all():
-            try:
-                home_page = HomePage.objects.get(locale=locale)
-            except HomePage.DoesNotExist:
-                home_page = HomePage(
-                    locale=locale,
-                    title=default_home.title,
-                    translation_key=default_home.translation_key,
-                )
-                default_home.get_parent().add_child(instance=home_page)
-                home_page.save_revision().publish()
-        for home in HomePage.objects.all():
-            sip = home.get_children().filter(title="Sections").exists()
-            if not sip:
-                new_sip = SectionIndexPage(title="Sections")
-                home.add_child(instance=new_sip)
-                new_sip.save_revision().publish()
--- articles/management/commands/import_sections.py
+++ articles/management/commands/import_sections.py
@@ -1,5 +1,6 @@
 import json
 
+from django.core.exceptions import MultipleObjectsReturned
 from django.core.management.base import BaseCommand
 from wagtail.core.models import Locale
 from wagtail.images.models import Image
@@ -12,12 +13,54 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open("sections.json", "r", encoding="utf-8") as f:
             sections = json.load(f)
-            SectionPage.objects.all().delete()
             for section in sections.keys():
-                locale = Locale.objects.get(language_code=sections[section]["locale"])
+                try:
+                    locale = Locale.objects.get(
+                        language_code=sections[section]["locale"]
+                    )
+                except MultipleObjectsReturned:
+                    self.stdout.write(
+                        self.style.WARNING(
+                            "Multiple Locales found: "
+                            + str(sections[section]["locale"])
+                        )
+                    )
+                except Locale.DoesNotExist:
+                    self.stdout.write(
+                        self.style.WARNING(
+                            "Locale does not exist: " + str(sections[section]["locale"])
+                        )
+                    )
+                try:
+                    home_page = HomePage.objects.get(
+                        title__icontains=sections[section]["main_title"], locale=locale
+                    )
+                except MultipleObjectsReturned:
+                    self.stdout.write(
+                        self.style.WARNING(
+                            "Multiple home pages found: "
+                            + str(sections[section]["main_title"])
+                        )
+                    )
+                except HomePage.DoesNotExist:
+                    self.stdout.write(
+                        self.style.WARNING(
+                            "Home page does not exist: "
+                            + str(sections[section]["main_title"])
+                        )
+                    )
                 try:
                     new_section = SectionPage.objects.get(
-                        locale=locale, title=sections[section]["title"]
+                        locale=locale,
+                        title=sections[section]["title"],
+                        site__pk=home_page.site.pk,
+                    )
+                except MultipleObjectsReturned:
+                    self.stdout.write(
+                        self.style.WARNING(
+                            "Multiple section pages found: "
+                            + str(sections[section]["title"])
+                        )
                     )
                 except SectionPage.DoesNotExist:
                     new_section = SectionPage(
@@ -38,11 +81,17 @@ class Command(BaseCommand):
                         trans_locale = sections[str(trans_pks[0])]["locale"]
                         try:
                             trans_section = SectionPage.objects.get(
-                                slug=trans_slug, locale__language_code=trans_locale
+                                slug=trans_slug,
+                                locale__language_code=trans_locale,
+                                site__pk=home_page.site.pk,
                             )
                             new_section.translation_key = trans_section.translation_key
                         except SectionPage.DoesNotExist:
-                            pass
+                            self.stdout.write(
+                                self.style.WARNING(
+                                    "Translation does not exist: " + str(trans_slug)
+                                )
+                            )
                     if "section_parent_pk" in sections[section].keys():
                         parent_slug = sections[sections[section]["section_parent_pk"]][
                             "slug"
@@ -51,14 +100,14 @@ class Command(BaseCommand):
                             sections[section]["section_parent_pk"]
                         ]["locale"]
                         parent = SectionPage.objects.get(
-                            slug=parent_slug, locale__language_code=parent_locale
+                            slug=parent_slug,
+                            locale__language_code=parent_locale,
+                            site=home_page.site,
                         )
                     else:
-                        parent = (
-                            HomePage.objects.get(locale=locale)
-                            .get_children()
-                            .filter(title="Sections")
-                            .first()
-                        )
+                        parent = home_page
                     parent.add_child(instance=new_section)
-                    new_section.save_revision().publish()
+                    if sections[section]["is_live"]:
+                        new_section.save_revision().publish()
+                    else:
+                        new_section.save_revision()
