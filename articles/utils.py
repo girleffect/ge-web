2feb1208d30be735981db2553a6f655a49e93c0a
Codie Roelf <codiebeulaine@gmail.com>
2022-02-18 06:12:57 +0200

    cleanup

--- articles/utils.py
+++ articles/utils.py
@@ -2,7 +2,4 @@ from django.core.paginator import Paginator
 
 
 def paginate(articles, page=1):
-    results = Paginator(articles, 10)
-    page_number = page
-    page_obj = results.get_page(page_number)
-    return page_obj
+    return Paginator(articles, 10).get_page(page)
