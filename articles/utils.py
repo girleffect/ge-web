bef0039a84580c8c9e230ec775c05b2b07d50064
Mitso <mqalaba@gmail.com>
2022-04-04 15:47:45 +0200

    update yegna features

--- articles/utils.py
+++ articles/utils.py
@@ -12,14 +12,16 @@ def filesizeformat(bytes):
     """
     try:
         bytes = float(bytes)
-    except (TypeError,ValueError,UnicodeDecodeError):
-        return u"0 bytes"
+    except (TypeError, ValueError, UnicodeDecodeError):
+        return "0 bytes"
 
     if bytes < 1024:
-        return ungettext("%(size)d byte", "%(size)d bytes", bytes) % {'size': bytes}
+        return ungettext("%(size)d byte", "%(size)d bytes", bytes) % {"size": bytes}
     if bytes < 1024 * 1024:
         return ugettext("%.1f KB") % (bytes / 1024)
     if bytes < 1024 * 1024 * 1024:
         return ugettext("%.1f MB") % (bytes / (1024 * 1024))
     return ugettext("%.1f GB") % (bytes / (1024 * 1024 * 1024))
-filesizeformat.is_safe = True 
+
+
+filesizeformat.is_safe = True
