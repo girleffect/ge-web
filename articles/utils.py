27ccb14de930ed4c041bc025a114decb1586286a
Mitso <mqalaba@gmail.com>
2022-04-04 16:35:21 +0200

    remove filesizeformat article

--- articles/utils.py
+++ articles/utils.py
@@ -1,32 +1,5 @@
-from django.utils.translation import (
-    ungettext,
-    ugettext,
-    ugettext_lazy as _,
-)
 from django.core.paginator import Paginator
 
 
 def paginate(articles, page=1):
     return Paginator(articles, 10).get_page(page)
-
-
-def filesizeformat(bytes):
-    """
-    Formats the value like a 'human-readable' file size (i.e. 13 KB, 4.1 MB,
-    102 bytes, etc).
-    """
-    try:
-        bytes = float(bytes)
-    except (TypeError, ValueError, UnicodeDecodeError):
-        return "0 bytes"
-
-    if bytes < 1024:
-        return ungettext("%(size)d byte", "%(size)d bytes", bytes) % {"size": bytes}
-    if bytes < 1024 * 1024:
-        return ugettext("%.1f KB") % (bytes / 1024)
-    if bytes < 1024 * 1024 * 1024:
-        return ugettext("%.1f MB") % (bytes / (1024 * 1024))
-    return ugettext("%.1f GB") % (bytes / (1024 * 1024 * 1024))
-
-
-filesizeformat.is_safe = True
