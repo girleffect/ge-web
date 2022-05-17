db5848f300488d6d76ec02d3c3cf8e58b3888ec2
megh-k <meghan@praekelt.org>
2022-05-17 10:15:31 +0200

    update oauth login permissions to get site from request

--- geweb/pipeline/set_permissions.py
+++ geweb/pipeline/set_permissions.py
@@ -11,15 +11,15 @@ def set_permissions(user, is_new, *args, **kwargs):
         user.save()
 
 
-def auth_allowed(user, backend, details, response, *args, **kwargs):
-    _update_auth_whitelisted_emails()
-    if not backend.auth_allowed(response, details):
+def auth_allowed(user, backend, details, response, request, *args, **kwargs):
+    _update_auth_whitelisted_emails(request)
+    if not backend.auth_allowed(response, details) or not settings.SOCIAL_AUTH_WHITELISTED_EMAILS:
         user.delete()
         raise AuthForbidden(backend)
 
 
-def _update_auth_whitelisted_emails():
-    site = Site.objects.first()
+def _update_auth_whitelisted_emails(request):
+    site = Site.find_for_request(request)
     site_settings = SiteSettings.for_site(site)
     settings.SOCIAL_AUTH_WHITELISTED_EMAILS += [
         str(email)
